8d048ad624a2c3276208374ef22dd99e5d79f7b3
Admin <admin@anass-iMac.local>
2020-04-25 13:18:28 +0200

    ui layout

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,5 +1,37 @@
 from django.shortcuts import render
 
 # Create your views here.
+posts =[
+            {
+            'title': 'التدوينه الاولي',
+            'content': 'نص التدوينه الاولي كنص تجريبي',
+            'post_date': '15-3-2019',
+            'author': 'Ahmad Abouissa'
+            },
+
+            {
+            'title': 'التدوينه الثانيه',
+            'content': 'نص التدوينه الاولي كنص تجريبي',
+            'post_date': '15-3-2019',
+            'author': 'Ahmad Abouissa'
+            },
+            {
+            'title': 'التدوينه الثالثه',
+            'content': 'نص التدوينه الاولي كنص تجريبي',
+            'post_date': '15-3-2019',
+            'author': 'Ahmad Abouissa'
+            },
+            {
+            'title': 'التدوينه الرابعه',
+            'content': 'نص التدوينه الاولي كنص تجريبي',
+            'post_date': '15-3-2019',
+            'author': 'Ahmad Abouissa'
+            },
+]
 def home(request):
-    return render(request, 'blog/index.html',{'title':'home'})
+    context={
+    'title': 'الصفحه الرئيسيه',
+    'posts': posts
+        
+    }
+    return render(request, 'blog/index.html',context)
